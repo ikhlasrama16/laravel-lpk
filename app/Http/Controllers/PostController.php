691a6b06309deaee54ab12f7f5f0813f5f9e5a67
ikhlasrama16 <ikhlasrama16@gmail.com>
2024-08-28 17:05:18 +0700

    commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,5 +12,13 @@ class PostController extends Controller
         $description = $request->description;
         $dom = new DOMDocument();
         $dom->loadHTML($description, 9);
+
+        $images = $dom->getElementsByTagName('img');
+
+        foreach ($images as $key => $img) {
+            $data = base64_decode(explode(',',explode(';', $img->getAttribute('src'))[1])[1]);
+            $images_name = "/upload/" . time(). $key.'png';
+            file_put_contents(public_path().$images_name, $data);
+        }
     }
 }

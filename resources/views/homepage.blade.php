ad8527314927ae3ebd19e361623122f779209ec9
ikhlasrama16 <ikhlasrama16@gmail.com>
2024-09-02 08:26:25 +0700

    commit

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -37,7 +37,7 @@
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown link
+                        Bahasa
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                         <li>
@@ -83,27 +83,9 @@
 
     <section id="hero">
         <div class="container">
-            <h1 class="poppins-medium">coba coba</h1>
+            <h1 class="poppins-medium">Homepage</h1>
         </div>
     </section>
-
-    <section>
-        <div>
-            <table border="1">
-                <tr>
-                    <td>Nama</td>
-                    <td>Email</td>
-                </tr>
-                @foreach ($data as $data)
-                    <tr>
-                        <td>{{ $data->name }}</td>
-                        <td>{{ $data->email }}</td>
-                    </tr>
-                @endforeach
-            </table>
-        </div>
-    </section>
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>

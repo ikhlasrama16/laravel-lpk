9b887d48fa67c4b323de7cd1cc5c9e6925662137
ikhlasrama16 <ikhlasrama16@gmail.com>
2024-09-05 17:02:30 +0700

    summernote backend

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storrage;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PostController extends Controller
 {
@@ -26,6 +29,45 @@ class PostController extends Controller
         ];
 
         $this->validate($request, $rules, $messages);
+
+
+        //image
+        $fileName = time().'.'.$request->image->extention();
+        $request->file('image')->storeAs('public/artikel', $fileName);
+
+        //artikel
+        $storage = "storage/content-artikel";
+        $dom = new \DOMDocument();
+        libxml_use_internal_errors(true);
+        $dom->loadHTML($request->desc, LIBXML_HTML_NOIMPLIED|LIBXML_HTML_NOIMPLIED);
+        libxml_clear_errors();
+
+        $images = $dom->getElementsByTagName('img');
+
+        foreach($images as $img){
+            $src = $img->getAttribute('src');
+            if (preg_match(`/data:image/`, $src)) {
+                preg_match(`/data:image\/(?<mime>.*?)\;/`, $src, $groups);
+                $mimetype = $groups['mime'];
+                $fileNameContent = uniqid();
+                $fileNameContentRand = substr(md5($fileNameContent),6,6).'_'.time();
+                $filePath = ("$storage/$fileNameContentRand.$mimetype");
+                $image = new Image::make($src)->resize(1200, 1200)->encode($mimetype, 100)->save(public_path($filePath));
+                $new_src = asset($filePath);
+                $img->removeAttribute('src');
+                $img->setAttribute('src', $new_src);
+                $img->setAttribute('class', 'img-responsive');
+
+            }
+        }
+
+        Post::create([
+            'title'=> $request->title,
+            'image'=> $fileName,
+            'desc'=> $dom->saveHTML(),
+        ])
+
+
         return redirect('admin.blog.cfreate')->with('success', 'data berhasil di simpan');
     }
 

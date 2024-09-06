1b8880b8b9442af02c4cf78310a4fb8727593d4f
ikhlasrama16 <ikhlasrama16@gmail.com>
2024-09-06 16:52:53 +0700

    commit unfinished

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,72 +5,73 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storrage;
+use Illuminate\Support\Facades\Storage;
 use Intervention\Image\ImageManagerStatic as Image;
 
 class PostController extends Controller
 {
-
     public function add_blog(){
         return view('admin.blog.create');
     }
 
     public function post(Request $request){
-        $rules=[
-            'title' => 'required',
-            'image' => 'required|max:1000|mimes:jpg, png, jpeg',
-            'desc' => 'required|min:20'
-        ];
+    $rules = [
+        'title' => 'required',
+        'image' => 'required|mimes:jpg,jpeg,png|max:1000', // Pemisah koma yang benar tanpa spasi
+        'description' => 'required|min:20'
+    ];
 
-        $messages = [
-            'title.required' => 'judul wajib di isi',
-            'image.required' => 'gambar wajib di upload',
-            'desc.required' => 'deskripsi wajib di isi',
-        ];
+    $messages = [
+        'title.required' => 'judul wajib di isi',
+        'image.required' => 'gambar wajib di upload',
+        'description.required' => 'deskripsi wajib di isi',
+    ];
 
-        $this->validate($request, $rules, $messages);
+    $this->validate($request, $rules, $messages);
 
+    // Proses gambar utama
+    $fileName = time() . '.' . $request->file('image')->extension();
+    $request->file('image')->storeAs('public/artikel', $fileName); // Menyimpan gambar utama
 
-        //image
-        $fileName = time().'.'.$request->image->extention();
-        $request->file('image')->storeAs('public/artikel', $fileName);
+    // Manipulasi konten gambar pada Summernote
+    $storage = "storage/content-artikel";
+    $dom = new \DOMDocument();
+    libxml_use_internal_errors(true);
+    $dom->loadHTML($request->description, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NOIMPLIED);
+    libxml_clear_errors();
 
-        //artikel
-        $storage = "storage/content-artikel";
-        $dom = new \DOMDocument();
-        libxml_use_internal_errors(true);
-        $dom->loadHTML($request->desc, LIBXML_HTML_NOIMPLIED|LIBXML_HTML_NOIMPLIED);
-        libxml_clear_errors();
+    $images = $dom->getElementsByTagName('img');
 
-        $images = $dom->getElementsByTagName('img');
+    foreach ($images as $img) {
+        $src = $img->getAttribute('src');
+        if (preg_match('/data:image/', $src)) {
+            preg_match('/data:image\/(?<mime>.*?)\;/', $src, $groups);
+            $mimetype = $groups['mime'];
+            $fileNameContent = uniqid();
+            $fileNameContentRand = substr(md5($fileNameContent), 6, 6) . '_' . time();
+            $filePath = ("$storage/$fileNameContentRand.$mimetype");
 
-        foreach($images as $img){
-            $src = $img->getAttribute('src');
-            if (preg_match(`/data:image/`, $src)) {
-                preg_match(`/data:image\/(?<mime>.*?)\;/`, $src, $groups);
-                $mimetype = $groups['mime'];
-                $fileNameContent = uniqid();
-                $fileNameContentRand = substr(md5($fileNameContent),6,6).'_'.time();
-                $filePath = ("$storage/$fileNameContentRand.$mimetype");
-                $image = new Image::make($src)->resize(1200, 1200)->encode($mimetype, 100)->save(public_path($filePath));
-                $new_src = asset($filePath);
-                $img->removeAttribute('src');
-                $img->setAttribute('src', $new_src);
-                $img->setAttribute('class', 'img-responsive');
+            // Proses penyimpanan gambar dari summernote
+            $image = \Image::make($src)
+                ->resize(1200, 1200) // Resize jika diperlukan
+                ->encode($mimetype, 100)
+                ->save(public_path($filePath));
 
-            }
+            $new_src = asset($filePath);
+            $img->removeAttribute('src');
+            $img->setAttribute('src', $new_src);
+            $img->setAttribute('class', 'img-responsive');
         }
-
-        Post::create([
-            'title'=> $request->title,
-            'image'=> $fileName,
-            'desc'=> $dom->saveHTML(),
-        ])
-
-
-        return redirect('admin.blog.cfreate')->with('success', 'data berhasil di simpan');
     }
 
+    // Penyimpanan data ke dalam database
+    Post::create([
+        'title' => $request->title,
+        'image' => $fileName,
+        'description' => $dom->saveHTML(),
+    ]);
 
+    return redirect()->route('admin.blog.create')->with('success', 'Data berhasil disimpan');
+}
 
 }
--- resources/views/admin/blog/create.blade.php
+++ resources/views/admin/blog/create.blade.php
@@ -20,15 +20,14 @@
     <!-- Main content -->
     <section class="">
         <div class="container-fluid">
-
                 <!-- left column -->
                  <div class="col-md-11 rounded-5">
                     <!-- jquery validation -->
                      <div class="card bg-white p-4">
                         <!-- /.card-header --><!-- form start -->
-                         <form id="quickForm" action="{{ route('admin.post') }}" method="POST">
+                         <form action="{{ route('admin.post') }}" id="quickForm" method="POST">
                             @csrf
-                            <div class="form-group">
+                            <div class="form-group mb-3">
                                 <label for="exampleInputEmail1">Judul</label>
                                 <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" placeholder="Enter title">
                                 @error('title')
@@ -37,26 +36,23 @@
                                     </div>
                                 @enderror
                             </div>
-                            <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
-                            </div>
-                            <div class="custom-file">
-                                <input type="file" name="image" class="custom-file-input @error('image') is-invalid @enderror" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
-                                <label class="custom-file-label form-control" for="inputGroupFile01">Choose file</label>
+                            <div class="form-group mb-3">
+                                <label for="">Upload Image</label>
+                                <input type="file" class="form-control" name="image">
                                 @error('image')
                                 <div class="invalid-feedback">
                                     {{ $message }}
                                 </div>
                                 @enderror
                             </div>
+                            <div class="form-group mb-3">
+                                <textarea id="description" name="description" class="form-control @error('description') is-invalid @enderror"></textarea>
+                                @error('description')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
                             </div>
-                            <textarea id="description" name="desc" class="form-control @error('desc') is-invalid @enderror"></textarea>
-                            @error('desc')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                            @enderror
                             <!-- /.card-body -->
 
                             <button type="submit" class="btn btn-primary mt-5">Submit</button>

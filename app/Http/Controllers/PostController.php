de57f26b73f03d3f54d87840299007a25585a4d7
ikhlasrama16 <ikhlasrama16@gmail.com>
2024-09-12 16:32:54 +0700

    update post controller unfinished

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -79,5 +79,83 @@ class PostController extends Controller
         return view('admin.blog.edit', ['artikel' => $artikel]);
     }
 
+    public function update(Request $request, $id)
+    {
+        $artikel = Post::find($id);
+
+        if ($request->hasFile('image')) {
+            $fileCheck = 'required|image|mimes:jpeg,png,jpg|max:2048';
+        }else{
+            $fileCheck = 'image|mimes:jpeg,png,jpg|max:2048';
+        }
+
+
+        $rules = [
+            'title' => 'required',
+            'image' => $fileCheck,
+            'description' => 'required|min:20',
+        ];
+
+        $messages = [
+            'title.required' => 'Judul wajib diisi!',
+            'image.required' => 'Gambar wajib diisi!',
+            'description.required' => 'Deskripsi wajib diisi!',
+        ];
+
+        if ($request->hasFile('image')) {
+            if (\File::exists('storage/artikel/'.$artikel->image)) {
+                \File::delete('storage/artikel/'.$artikel->image);
+            }
+            $fileName = time().'.'.$request->image->exstension();
+            $request->file('image')->storeAs('public/artikel', $fileName);
+        }
+
+
+
+        $this->validate($request, $rules, $messages);
+
+        // Image
+        $fileName = time() . '.' . $request->image->extension();
+        $request->file('image')->storeAs('public/artikel', $fileName);
+
+        # Artikel
+        $storage = "storage/content-artikel";
+        $dom = new \DOMDocument();
+
+        # untuk menonaktifkan kesalahan libxml standar dan memungkinkan penanganan kesalahan pengguna.
+        libxml_use_internal_errors(true);
+        $dom->loadHTML($request->description, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NOIMPLIED);
+        # Menghapus buffer kesalahan libxml
+        libxml_clear_errors();
+
+        # Baca di https://dosenit.com/php/fungsi-libxml-php
+        $images = $dom->getElementsByTagName('img');
+
+        foreach ($images as $img) {
+            $src = $img->getAttribute('src');
+            if (preg_match('/data:image/', $src)) {
+                preg_match('/data:image\/(?<mime>.*?)\;/', $src, $groups);
+                $mimetype = $groups['mime'];
+                $fileNameContent = uniqid();
+                $fileNameContentRand = substr(md5($fileNameContent), 6, 6) . '_' . time();
+                $filePath = ("$storage/$fileNameContentRand.$mimetype");
+                $image = Image::make($src)->resize(1440, 720)->encode($mimetype, 100)->save(public_path($filePath));
+                $new_src = asset($filePath);
+                $img->removeAttribute('src');
+                $img->setAttribute('src', $new_src);
+                $img->setAttribute('class', 'img-responsive');
+            }
+        }
+
+        Post::create([
+            'title' => $request->title,
+            'image' => $fileName,
+            'description' => $dom->saveHTML(),
+        ]);
+
+        return redirect(route('admin.blog'))->with('success', 'data berhasil di simpan');
+
+    }
+
 
 }

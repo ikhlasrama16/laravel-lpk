802a6bd050bb9c686a611ca860e9b3e3f335b150
ikhlasrama16 <ikhlasrama16@gmail.com>
2024-09-24 15:42:47 +0700

    section program

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -25,19 +25,30 @@
         <div id="program" class="container py-5 col-xxl-10">
             <div class="row center-content">
                 <div class="col">
-                    <img src="{{ asset('assets/background/program-homepage.jpg') }}" alt="" class="rounded w-75">
+                    <img src="{{ asset('assets/content/program-homepage.jpg') }}" alt="Program Homepage"
+                        class="rounded w-75">
                 </div>
                 <div class="col">
                     <h2 class="poppins-bold">Apa saja Progam Magang di<br> LPK Mandiri Nusantara?</h2>
-                <div class="stripe"></div>
+                    <div class="stripe"></div>
+                    <p>Tokutei Ginou 特定技能 (kontrak dan visa kerja 5 tahun)</p>
+                    <div class="hover-container">
+                        <div class="hover-content">
+                            <img src="{{ asset('assets/logo/pertanian-icon.png') }}" alt="Pertanian Icon" class="hover-image mb-3">
+                            <h4 class="ms-3 poppins-bold">Bagian Keperawatan Lansia</h4>
+                        </div>
+                        <p class="hover-text">Di program ini Anda akan mendapatkan kontrak kerja dan visa kerja selama 5 tahun untuk bagian Keperawatan Lansia.</p>
+                    </div>
+                    <div class="hover-container">
+                        <div class="hover-content">
+                            <img src="{{ asset('assets/logo/perawat-icon.png') }}" alt="Perawat Icon" class="hover-image mb-3">
+                            <h4 class="ms-3 poppins-bold">Bagian Pertanian</h4>
+                        </div>
+                        <p class="hover-text">Di program ini Anda akan mendapatkan kontrak kerja dan visa kerja selama 5 tahun untuk bagian pertanian.</p>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div id="mengapa">
-            <div class="row">
-
-            </div>
-        </div>
     </section>
 @endsection

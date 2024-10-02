c6fab297a4ecbc2e83e90b36830881ca803939fb
ikhlasrama16 <ikhlasrama16@gmail.com>
2024-10-02 16:42:18 +0700

    initial program

--- resources/views/program.blade.php
+++ resources/views/program.blade.php
@@ -3,6 +3,9 @@
     <section id="program" class="mt-5 py-5">
         <div class="container">
             <h1 class="poppins-bold">Program</h1>
+            <div>
+                <h2>Tokutei Ginou 特定技能 ( Kontrak dan Visa Kerja 5 Tahun )</h2>
+            </div>
         </div>
     </section>
 @endsection

6ead1b23fb4bc8577a6448b26f7230552b9d666e
ikhlasrama16 <ikhlasrama16@gmail.com>
2024-10-02 14:21:16 +0700

    about page

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts')
 @section('content')
 <section id="gallery" class="mt-5 py-5">
-    <div>
-
+    <div class="container">
+        <h1 class="poppins-bold">Gallery</h1>
     </div>
 </section>
 @endsection

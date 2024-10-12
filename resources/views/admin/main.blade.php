031adb0f4882c9013d042b00fcebb1803181cae9
ikhlasrama16 <ikhlasrama16@gmail.com>
2024-10-12 15:00:08 +0700

    gallery management admin

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -85,6 +85,14 @@
                         </p>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-image"></i>
+                        <p>
+                            Manajemen Galeri
+                        </p>
+                    </a>
+                </li>
             </ul>
         </nav>
         <!-- /.sidebar-menu -->

baef8e9f1db00d19c1075ab896d0a890f169f0ad
ikhlasrama16 <ikhlasrama16@gmail.com>
2024-10-12 08:30:19 +0700

    blog detail - user

--- resources/views/homepage/blogdetail.blade.php
+++ resources/views/homepage/blogdetail.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts')
 @section('content')
     <section id="detail" class="mt-5 py-5">
-        <div class="container col-xxl-10 py-5">
+        <div class="container col-md-6 py-5">
             <h3 class="fw-bold mb-3">
                 {{ $artikel->title }}
             </h3>

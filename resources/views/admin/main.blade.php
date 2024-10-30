aa3bfb38732f02e853cc3ee5c2cfb10bb0222cd6
ikhlasrama16 <ikhlasrama16@gmail.com>
2024-10-30 16:25:31 +0700

    update admin title

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>Karirdijepang | Admin </title>
   <link rel="shortcut icon" href="{{ asset("assets/logo/favicon.ico") }}">
   @include('includes.admin.style')
 </head>
